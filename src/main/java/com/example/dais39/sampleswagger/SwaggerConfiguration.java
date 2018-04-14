7ac20eed383467beb63f258b318409f11058d480
Daisaku Suzuki <ds.suzuki217@gmail.com>
2018-04-14 21:36:56 +0900

    swagger specificationのvalidationを無効にする

--- src/main/java/com/example/dais39/sampleswagger/SwaggerConfiguration.java
+++ src/main/java/com/example/dais39/sampleswagger/SwaggerConfiguration.java
@@ -1,9 +1,19 @@
 package com.example.dais39.sampleswagger;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
+
+    @Bean
+    public UiConfiguration uiConfiguration() {
+        return UiConfigurationBuilder.builder()
+                .validatorUrl("")
+                .build();
+    }
 }
